5cb6990888bb89f7683642af6adc6debf1e31e98
Marco Montilla <marco.jr1695@gmail.com>
2019-10-28 09:31:58 -0400

    Sing In/out component and Sign In Section

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SignIn from "../../components/sing-in/sing-in.component";
+
+import "./sign-in-and-sign-up.styles.scss";
+
+const SignInAndSignUp = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInAndSignUp;
